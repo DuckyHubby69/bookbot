bbbd219bfef72d8fb50089c3f6953c43016a7a4e
mike101 <mikeyrov@gmail.com>
2025-04-23 17:30:03 -0400

    Updated main.py and added stats.py file for modularity

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+from stats import count_words
 def get_book_text(book_path):
     with open(book_path) as f:
         file_contents = f.read()
     return file_contents
-def count_words(book_contents):
-    book_words = book_contents.split()
-    word_count = len(book_words)
-    return word_count
 def main():
     book_contents = get_book_text("books/frankenstein.txt")
     book_word_count = count_words(book_contents)
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(book_contents):
+    book_words = book_contents.split()
+    word_count = len(book_words)
+    return word_count
