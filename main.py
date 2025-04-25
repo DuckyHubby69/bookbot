9a93f868ff7791711a443ef4c591d4f356cba2c0
mike101 <mikeyrov@gmail.com>
2025-04-24 20:02:45 -0400

    Update main.py and stats.py for more function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 def get_book_text(book_path):
     with open(book_path) as f:
         file_contents = f.read()
@@ -7,4 +8,7 @@ def main():
     book_contents = get_book_text("books/frankenstein.txt")
     book_word_count = count_words(book_contents)
     print(f"{book_word_count} words found in the document")
+    book_character_counts = count_characters(book_contents)
+    for char, count in book_character_counts.items():
+        print(f"'{char}': {count}")
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def count_words(book_contents):
     book_words = book_contents.split()
     word_count = len(book_words)
     return word_count
+def count_characters(book_contents):
+    counts = {}
+    lower_case = book_contents.lower()
+    for char in lower_case:
+        if char in counts:
+            counts[char] = counts[char] + 1
+        else:
+            counts[char] = 1
+    return counts
